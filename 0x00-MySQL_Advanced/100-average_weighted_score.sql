a505c44d419484db1abbea7e4d2b90085d702842
Sunkade <sunkadefasola@gmail.com>
2024-03-22 00:28:42 +0000

    11-need_meeting.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,10 +1,15 @@
--- creates a view need_meeting that lists all students that have a
--- score under 80 (strict) and no last_meeting or more than 1 month.
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
+-- That computes and store the average weighted score for a student
 
-DROP VIEW IF EXISTS need_meeting;
-
-CREATE VIEW need_meeting AS
-SELECT name 
-FROM students 
-WHERE score < 80 AND 
-(last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
+DROP procedure IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER |
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
+	IN user_id INT
+)
+BEGIN
+    UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+END;
+|
